52df53328d6d3d5b547d104cd06263ef1c20d61b
Owner <owner@Owners-MacBook-Pro.local>
2020-05-02 18:24:34 -0400

    Added partially completeTree ADTs with preorder, postorder, inorder and levelorder traversals. Based on Problem Solving w/ Data Structures and Algorithms in Python 2e

---
+++ Trees/parse_tree.py
@@ -0,0 +1,18 @@
+"""
+    Purpose:    Write a program to parse a fully parenthesized mathematical 
+                expression and then evaluate it
+                Extra challenge - can you conver a non fpexp into a fpexp
+    Filename:   parse_tree.py
+    Author:     Siddharth Kapoor
+    Date:       May 2, 2020
+"""
+
+def build_parsetree(fpexp):
+    """
+    args:   fpexp is a fully parenthesized expression for example
+            ((3 * 8) + (4 / 2))
+    """
+    pass
+
+def evaluate(parsetree):
+    pass
